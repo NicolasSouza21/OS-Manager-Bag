5bb4528801701e2ffabbed8b74e9acb34761816c
NicolasSouza21 <algumacoisan@gmail.com>
2025-06-24 17:09:27 -0300

    24/06/2025

---
+++ src/main/java/com/bag/osmanager/OsmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.bag.osmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bag/osmanager/OsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bag.osmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
