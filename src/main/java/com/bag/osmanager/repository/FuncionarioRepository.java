1a7536e44d919bbaac15f344d0f7a67499e31407
NicolasSouza21 <algumacoisan@gmail.com>
2025-06-25 07:57:20 -0300

    aaaaaaaaaaaaaaaaaa

--- src/main/java/com/bag/osmanager/repository/FuncionarioRepository.java
+++ src/main/java/com/bag/osmanager/repository/FuncionarioRepository.java
@@ -4,5 +4,4 @@ import com.bag.osmanager.model.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-    // Podemos adicionar métodos de busca customizados depois (ex: por tipo de funcionário)
 }
